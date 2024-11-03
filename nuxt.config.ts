c549272bfbc94ad8bfd3617f58a292109542cbd6
Justin Chen <interjc@live.com>
2024-11-03 12:21:09 +0900

    :art: update og meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,13 +16,17 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: '从零开始学习 Web 开发，掌握前端技术栈，完成项目从开发到上线的全过程' },
+        { property: 'og:url', content: 'https://justincourse.com/' },
+        { property: 'og:type', content: 'website' },
         { property: 'og:title', content: 'Web 开发从入门到上线' },
         { property: 'og:description', content: '从零开始学习 Web 开发，掌握前端技术栈，完成项目从开发到上线的全过程' },
         { property: 'og:image', content: '/og-image-0.jpg' },
-        { property: 'og:type', content: 'website' },
         { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'twitter:site', content: '@interjc' },
-        { name: 'twitter:creator', content: '@interjc' }
+        { property: 'twitter:domain', content: 'justincourse.com' },
+        { property: 'twitter:url', content: 'https://justincourse.com/' },
+        { name: 'twitter:title', content: 'Web 开发从入门到上线' },
+        { name: 'twitter:description', content: '从零开始学习 Web 开发，掌握前端技术栈，完成项目从开发到上线的全过程' },
+        { name: 'twitter:image', content: '/og-image-0.jpg' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
