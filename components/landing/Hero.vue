f948508ebad6af8c227c24acd5ede7ae3839b4f3
Justin Chen <interjc@live.com>
2024-11-10 17:30:09 +0900

    :alien: add new platform for course

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -27,32 +27,15 @@ const handleVideoEnded = () => {
 <template>
   <main class="grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-8">
     <div class="p-24 md:order-1 hidden md:block relative group">
-      <div
-        class="w-[384px] h-[384px] hover:scale-125 relative transition-transform"
+      <div class="w-[384px] h-[384px] hover:scale-125 relative transition-transform"
         @mouseover="hasInteracted = true; shouldAutoplay = true; videoRef.currentTime = 0; videoRef.play()"
-        @mouseleave="hasInteracted = false"
-      >
-        <img
-          class="rounded-full absolute inset-0 w-full h-full object-cover"
-          :class="{ 'opacity-0': shouldAutoplay }"
-          src="/assets/img/hero.webp"
-          alt="Starship starts the engine"
-          loading="eager"
-          width="384"
-          height="384"
-        />
-        <video
-          ref="videoRef"
-          class="rounded-full w-[384px] h-[384px] object-cover absolute inset-0"
+        @mouseleave="hasInteracted = false">
+        <img class="rounded-full absolute inset-0 w-full h-full object-cover" :class="{ 'opacity-0': shouldAutoplay }"
+          src="/assets/img/hero.webp" alt="Starship starts the engine" loading="eager" width="384" height="384" />
+        <video ref="videoRef" class="rounded-full w-[384px] h-[384px] object-cover absolute inset-0"
           :class="{ 'opacity-100': shouldAutoplay, 'opacity-0': !shouldAutoplay }"
-          src="https://pictures.zhaikr.com/The_cat_wizard_fly_pass_through_.mp4"
-          muted
-          :loop="hasInteracted"
-          autoplay
-          playsinline
-          poster="/assets/img/hero.webp"
-          @ended="handleVideoEnded"
-        ></video>
+          src="https://pictures.zhaikr.com/The_cat_wizard_fly_pass_through_.mp4" muted :loop="hasInteracted" autoplay
+          playsinline poster="/assets/img/hero.webp" @ended="handleVideoEnded"></video>
       </div>
     </div>
 
@@ -61,25 +44,14 @@ const handleVideoEnded = () => {
         Web 开发从入门到上线
       </h1>
       <p class="text-lg mt-4 text-slate-600 max-w-xl">
-        更多详情，请 <a target="_blank" href="https://indie-startups.notion.site/">点此查看</a>.
+        ✨ 更多详情，请
+        <a class="text-primary-500 hover:text-primary-700 dark:text-primary-400 dark:hover:text-primary-300 underline decoration-2 underline-offset-2" target="_blank" href="https://indie-startups.notion.site/">点此查看</a>
       </p>
       <div class="mt-6 flex flex-col sm:flex-row gap-3">
-        <LandingLink
-          href="https://buy.stripe.com/fZe9AW506dnr3IY6oq?prefilled_promo_code=2024DB11"
-          size="lg"
-          styleName="primary"
-          :block="false"
-          target="_blank"
-          rel="noopener"
-        >立即加入</LandingLink>
-        <LandingLink
-          href="https://indie-startups.notion.site/"
-          size="lg"
-          styleName="link"
-          :block="false"
-          target="_blank"
-          rel="noopener"
-        >课程简介</LandingLink>
+        <LandingLink href="https://buy.stripe.com/fZe9AW506dnr3IY6oq?prefilled_promo_code=2024DB11" size="lg"
+          styleName="primary" :block="false" target="_blank" rel="noopener">立即加入</LandingLink>
+        <LandingLink href="https://app.justincourse.com/web-course-phrase-1/" size="lg" styleName="link" :block="false"
+          target="_blank" rel="noopener">进入课程</LandingLink>
       </div>
     </div>
   </main>
--- components/landing/Navbar.vue
+++ components/landing/Navbar.vue
@@ -83,8 +83,8 @@ const isDarkMode = computed(() => {
               class="pt-1.5 text-gray-500 dark:text-slate-600 hover:text-gray-700 dark:hover:text-gray-400">
               <Icon name="ph:lightbulb-filament-bold" class="h-5 w-5" />
             </button>
-            <NuxtLink to="https://indie-startups.notion.site/" target="_blank" rel="noopener"
-              class="hover:text-gray-500 dark:hover:text-gray-300 transition-colors duration-300">课程简介</NuxtLink>
+            <NuxtLink to="https://app.justincourse.com/web-course-phrase-1/" target="_blank" rel="noopener"
+              class="hover:text-gray-500 dark:hover:text-gray-300 transition-colors duration-300">进入课程</NuxtLink>
             <LandingLink href="https://buy.stripe.com/fZe9AW506dnr3IY6oq?prefilled_promo_code=2024DB11" size="md"
               styleName="primary" target="_blank" rel="noopener" className="transition-colors duration-300">立即加入
             </LandingLink>
@@ -132,8 +132,9 @@ const isDarkMode = computed(() => {
             </li>
           </ul>
           <div class="flex items-center mt-3 gap-4">
-            <LandingLink href="https://indie-startups.notion.site/" size="md" :block="true" styleName="muted"
-              target="_blank" rel="noopener" className="hover:text-gray-500 transition-colors duration-300">课程简介
+            <LandingLink href="https://app.justincourse.com/web-course-phrase-1/" size="md" :block="true"
+              styleName="muted" target="_blank" rel="noopener"
+              className="hover:text-gray-500 transition-colors duration-300">进入课程
             </LandingLink>
             <LandingLink href="https://buy.stripe.com/fZe9AW506dnr3IY6oq?prefilled_promo_code=2024DB11" size="md"
               :block="true" styleName="primary" target="_blank" rel="noopener"
