25185ba3a5124f7ecabeac59c2b2d1f680c8ab10
ephao <gzhdong@foxmail.com>
2024-12-07 22:23:36 -0800

    修复背景图没有填充footer底部的问题

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="relative min-h-screen">
+  <div class="relative min-h-screen flex flex-col">
     <div class="absolute inset-0 z-[-1] opacity-10">
-      <img src="/iLoveMeme.svg" alt="background" class="w-full h-full object-cover" />
+      <img src="/iLoveMeme.svg" alt="background" class="w-full h-full object-cover fixed" />
     </div>
     <CommonNavbar></CommonNavbar>
-    <CommonContainer>
+    <CommonContainer class="flex-grow">
       <slot></slot>
     </CommonContainer>
     <CommonFooter></CommonFooter>
