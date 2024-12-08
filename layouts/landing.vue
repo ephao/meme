9ee38925c32ddeadfea47c0fcedb1536cb94fdd5
ephao <gzhdong@foxmail.com>
2024-12-07 22:18:14 -0800

    在layout/landing.vue填充背景图

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -1,7 +1,12 @@
 <template>
-  <CommonNavbar></CommonNavbar>
-  <CommonContainer>
-    <slot></slot>
-  </CommonContainer>
-  <CommonFooter></CommonFooter>
+  <div class="relative min-h-screen">
+    <div class="absolute inset-0 z-[-1] opacity-10">
+      <img src="/iLoveMeme.svg" alt="background" class="w-full h-full object-cover" />
+    </div>
+    <CommonNavbar></CommonNavbar>
+    <CommonContainer>
+      <slot></slot>
+    </CommonContainer>
+    <CommonFooter></CommonFooter>
+  </div>
 </template>
