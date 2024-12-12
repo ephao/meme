071c4a7e11999c25a8f27b3671dd9e2154b108e7
ephao <gzhdong@foxmail.com>
2024-12-11 19:37:41 -0800

    修改footer样式

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,15 +1,21 @@
 <template>
   <footer class="my-20">
     <div class="common-social-links flex space-x-4 items-center justify-center mb-2">
-      <a title="关注 X@iLoooveMeme" href="https://twitter.com/intent/follow?screen_name=iLoooveMeme" target="_blank" rel="noopener noreferrer"
-        class="flex items-center text-slate-500 hover:text-gray-600 transition-colors duration-300">
-        <Icon name="mdi:twitter" class="text-2xl mr-2"/>
-        <span>@iLoooveMeme</span>
-      </a>
       <div class="flex items-center">
-        <Icon name="mdi:email" class="text-2xl text-slate-500 mr-2"/>
-        <span class="text-slate-500">ford889313@gmail.com</span>
+        <a title="关注 X@iLoooveMeme" href="https://twitter.com/intent/follow?screen_name=iLoooveMeme" target="_blank" rel="noopener noreferrer"
+          class="text-slate-500 hover:text-gray-600 transition-colors duration-300 flex items-center justify-center">
+          <Icon name="mdi:twitter" class="text-2xl"/>
+        </a>
       </div>
+      <div class="flex items-center">
+        <button @click="showEmail = !showEmail" class="text-slate-500 hover:text-gray-600 transition-colors duration-300 flex items-center justify-center">
+          <Icon name="mdi:email" class="text-2xl"/>
+        </button>
+        <span v-if="showEmail" class="ml-2 text-slate-500 text-base flex items-center leading-none">iLoooveMeme@outlook.com</span>
+      </div>
+      <!-- <div class="cursor-default">
+        <Icon name="mdi:email" class="text-2xl text-slate-500" title="ford889313@gmail.com"/>
+      </div> -->
       <!-- <a title="立即加入课程" href="https://buy.stripe.com/fZe9AW506dnr3IY6oq?prefilled_promo_code=XMAS2024"
         target="_blank" rel="noopener noreferrer"
         class="text-2xl text-slate-500 hover:text-gray-600 transition-colors duration-300">
@@ -26,3 +32,9 @@
     </p>
   </footer>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const showEmail = ref(false)
+</script>
